1a05fd39ce2e42698066b8ed1f533b51cb11433f
Federico-Gaiani <s309900@studenti.polito.it>
2025-04-25 11:46:56 +0200

    implementazione, test vuoto

--- src/SortingAlgorithm.hpp
+++ src/SortingAlgorithm.hpp
@@ -1,11 +1,56 @@
 #pragma once
-
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
 namespace SortLibrary {
+template<typename T>
+concept Sortable = requires(T& t) {
+    {t < t} -> std::convertible_to<bool>;
+};
+
+template<Sortable T>
+void BubbleSort(std::vector<T>& v)
+{
+	
+    const unsigned int n = v.size();
+
+	if (n == 0) return; // Gestione esplicita del caso vuoto
+
+    for(unsigned int i = 0; i < n - 1; i++)
+    {
+        for(unsigned int j = i + 1; j < n; j++)
+        {
+            if(v[j] < v[i]){
+				// swap:
+				T tmp = v[i];
+				v[i] = v[j];
+				v[j] = tmp;
+			}      
+        } 
+    }
+}
+/*
+// TODO --> implementare mio heapsort
+template<Sortable T>
+void HeapSortPersonal(std::vector<T>& v){
+	
+	
+}
+*/
+
+template<Sortable T>
+void HeapSort(std::vector<T>& v)
+{
+    
+	make_heap(v.begin(), v.end());
+    sort_heap(v.begin(), v.end());
+	
+}
+
+
 
 
 }
--- src_test/test_sorting_algorithm.hpp
+++ src_test/test_sorting_algorithm.hpp
@@ -8,20 +8,34 @@
 
 namespace SortLibrary {
 
-TEST(TestSorting, TestSelectionSort)
+TEST(TestSorting, TestBubbleSort)
 {
     std::vector<int> v = {9, 13, 5, 10, 2, 7, 9, 4, 6, 12};
-    SelectionSort<int>(v);
+    BubbleSort<int>(v);
     std::vector<int> sortedV = {2, 4, 5, 6, 7, 9, 9, 10, 12 ,13};
     EXPECT_EQ(v, sortedV);
 }
 
-TEST(TestSorting, TestInsertionSort)
+TEST(TestSorting, BubbleSortVuoto)
+{
+    std::vector<int> v; // vettore vuoto
+    BubbleSort<int>(v);
+    EXPECT_TRUE(v.empty()); 
+}
+
+TEST(TestSorting, TestHeapSort)
 {
     std::vector<int> v = {9, 13, 5, 10, 2, 7, 9, 4, 6, 12};
-    InsertionSort<int>(v);
+    HeapSort<int>(v);
     std::vector<int> sortedV = {2, 4, 5, 6, 7, 9, 9, 10, 12 ,13};
     EXPECT_EQ(v, sortedV);
+}
+
+TEST(TestSorting, HeapSortVuoto)
+{
+    std::vector<int> v; // vettore vuoto
+    HeapSort<int>(v);
+    EXPECT_TRUE(v.empty()); 
 }
 
 }
